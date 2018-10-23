685c61171fedbafa9e2e9018fe41c830f3ce35b2
Antoine Roy-Gobeil <antoine@plot.ly>
2018-10-22 22:54:45 -0400

    fix baseUrl for CircleCI

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  baseUrl: process.env.CI ? "/plotly-image-viewer/" : "/0/plotly-image-viewer/"
+  baseUrl: process.env.CI ? "/0/plotly-image-viewer/" : "/plotly-image-viewer/"
 };
